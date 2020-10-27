fd44aa5023f565473f8e62779c78a54a2a911266
Jay Nishant Gupta <guptaj2895@mayoboys.com>
2020-10-27 16:03:58 +0530

    Mistake Coming

--- screens/ReadStory.js
+++ screens/ReadStory.js
@@ -1,10 +1,56 @@
 import * as React from "react";
-import {View,Text} from 'react-native'
+import {View,Text} from 'react-native';
+import {SearchBar} from 'react-native-elements';
+import db from '../config';
+import firebase from 'firebase';
+import {FlatList} from 'react-native-gesture-handler';
 
 export default class Search extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      search:"",
+      allStories:[],
+    }
+  }
+
+  retriveStories = async() => {
+    const allStories = await db.collection('storyHub').get();
+    allStories.docs.map(()=>{
+      this.setState({
+        allStories:[this.state.allStories,doc.data()],
+      })
+    })
+  }
+
+  searchStory = async() => {
+    
+  }
+
   render(){
     return(
-      <Text style={{margin:120, fontSize:25}}>Read Story</Text>
+      <View>
+        <SearchBar
+          placeholder='Search...'
+          onChangeText={(text)=>{
+            this.setState({search:text});
+          }}
+          value={this.state.search}
+        />
+        <FlatList
+          data={this.state.allStories}
+          renderItem={({item})=>(
+            <View>
+                <Text>{'Author:' + item.author}</Text> 
+                <Text>{'Title:' + item.title}</Text> 
+                <Text>{'Story:' + item.story}</Text> 
+            </View>
+        )}
+          keyExtractor={(item,index)=>index.toString()}
+          onEndReached={this.retriveStories}
+          onEndReachedThreshold={0.7}
+        />
+      </View>
     )
   }
 }
